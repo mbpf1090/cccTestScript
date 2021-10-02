c07691f3a704b97bb4c4aa0ef0475ac099437ae9
mbpf1090 <sebastian.weidele@gmail.com>
2021-10-02 16:30:56 +0200

    download index.php

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ apt-get -qq update && apt-get -qq --assume-yes  install apache2
 apt-get -qq --assume-yes  install php libapache2-mod-php php-mysql
 
 rm /var/www/html/index.html
-touch /var/www/html/index.php
+#touch /var/www/html/index.php
 
-sh -c 'echo "<?php echo(getHostByName(getHostName())); ?>" >> /var/www/html/index.php'
+cd /var/www/html
+curl -LJO https://raw.githubusercontent.com/mbpf1090/cccTestScript/master/index.php --output index.php
+
+
+#sh -c 'echo "<?php echo(getHostByName(getHostName())); ?>" >> /var/www/html/index.php'
